0193df510acaf87055beec6c588dad49a0f16768
Shilpa Singh Thapa <shilpathapa001@gmail.com>
2022-03-20 17:56:40 +0545

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        
+        'first_name','last_name','status','phone_number','details'
     ];
 
     /**
